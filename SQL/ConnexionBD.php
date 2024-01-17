8013d08a107ca40804f23e15bd5dc97ab5322691
khalil-45 <khalil.abada@etu.univ-orleans.fr>
2024-01-17 21:06:15 +0100

    verif connexionBD

--- SQL/ConnexionBD.php
+++ SQL/ConnexionBD.php
@@ -1,37 +1,37 @@
 <?php
 
-class ConnexionBD {
+class ConnexionBD
+{
     private $connexion;
 
-    public function __construct() {
-        $this->connexion = new PDO('sqlite:../DBquiz.sqlite3');
-        $this->connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        if ($this->connexion == null){
-            echo 'Erreur de la connexion à la base de données';
+    public function __construct()
+    {
+        try {
+            $this->connexion = new PDO('sqlite:../DBquiz.sqlite3');
+            $this->connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo 'Connection failed: ' . $e->getMessage();
         }
-
     }
 
     public function get_connexion(): PDO
     {
-        return $this->connexion;
+        if ($this->connexion === null) {
+            throw new Exception('Connection failed');
+        }
 
+        return $this->connexion;
     }
+    
     public function deconnecter(): void
     {
         $this->connexion = null;
     }
 
-    public function drop_all() : void
+    public function drop_all(): void
     {
         $this->connexion->exec('DROP TABLE IF EXISTS QUESTION');
         $this->connexion->exec('DROP TABLE IF EXISTS TYPE');
         $this->connexion->exec('DROP TABLE IF EXISTS QUIZ');
     }
-
 }
-
-
-
-
-?>
